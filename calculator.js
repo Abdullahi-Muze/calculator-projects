e67983fbc2a53cdf5265649d93659668ba298290
Abdullahi-Muze <141625005+Abdullahi-Muze@users.noreply.github.com>
2024-02-05 18:35:10 +0200

    Update calculator.js
    waxan kusoo kordhiyey 
    1. in aan operatorka wax hal mar ka badan in uu kuu shaqayn karin

--- calculator.js
+++ calculator.js
@@ -8,24 +8,41 @@ let result=''
 buttons.forEach((btns)=>{
   btns.addEventListener('click', ()=>{
 
+
    
     if(btns.value ==='='){
-     
+    //  storeNumber= storeNumber.toString()
      result= eval(storeNumber)
-     dispaly.value =result
+     dispaly.value=result
      storeNumber=result;
+     console.log(result)
 
     }else if (btns.value === 'AC'){
       storeNumber=storeNumber.toString().slice(0, -1)
       dispaly.value= storeNumber
     }
     else{
-      storeNumber+= btns.value
-      dispaly.value= storeNumber
-      console.log(storeNumber)
+      if( btns.value === '+' & storeNumber.toString().slice(-1) === '+'||  btns.value === '*' & storeNumber.toString().slice(-1) === '*'||btns.value === '/' & storeNumber.toString().slice(-1) === '/'||btns.value === '-' & storeNumber.toString().slice(-1) === '-'){
+        
+        console.log('hi')
+        // storeNumber+= btns.value
+        // dispaly.value= storeNumber
+        // console.log(storeNumber)
+      }else{
+
+        storeNumber+= btns.value
+        dispaly.value= storeNumber
+        
+        // storeNumber= storeNumber.split(',')
+        console.log(storeNumber.slice(-1))
+        console.log(storeNumber)
+        }
+      // storeNumber+= btns.value
+      // dispaly.value= storeNumber
+      // console.log(storeNumber)
   
   }
-  console.log(storeNumber)
+  // console.log(typeof(storeNumber))
 
   })
 
@@ -41,3 +58,23 @@ buttons.forEach((btns)=>{
 // const resultWeb= website(2,4)
 // console.log(resultWeb)
 // const library= books(2,3)
+
+// let string= '1,2,3,4'
+// console.log(typeof(string))
+// console.log(string)
+
+// let ku= string.split(',')
+// console.log(typeof(ku))
+// console.log(ku)
+
+// console.log('dhexdhexaad')
+
+// let array= [1,2,3,4]
+// console.log(typeof(array))
+// console.log(array[2])
+
+// let ka= array.toString()
+// console.log(typeof(ka))
+// console.log(ka[2])
+
+
